be596899a116cf0acd3ea141e74b65914c2dff39
zonghewang <diaozhaofeng@126.com>
2025-12-20 09:19:52 +0800

    Update win-configure-security.ps1

--- .github/workflows/scripts/win-configure-security.ps1
+++ .github/workflows/scripts/win-configure-security.ps1
@@ -1,1 +1,1 @@
-secedit /configure /db secedit.sdb  /cfg $env:github.workspace/.github/workflows/config/security.cfg  /overwrite
+secedit /configure /db secedit.sdb  /cfg $env:WORK_SPACE/.github/workflows/config/security.cfg  /overwrite

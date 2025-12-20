3c1e3aa0d4ae7a84b5ef59a626acb822650ee818
zonghewang <diaozhaofeng@126.com>
2025-12-20 11:20:30 +0800

    Update win-install-tailscale.ps1

--- .github/workflows/scripts/win-install-tailscale.ps1
+++ .github/workflows/scripts/win-install-tailscale.ps1
@@ -3,6 +3,6 @@
 $tsUrl = "https://pkgs.tailscale.com/stable/tailscale-setup-1.92.3-amd64.msi"
 $installerPath = "$env:TEMP\tailscale.msi"
 Invoke-WebRequest -Uri $tsUrl -OutFile $installerPath
-Start-Process msiexec.exe -ArgumentList "/i","`"$installerPath`"","/quiet","/norestart" -Wait
+Start-Process msiexec.exe -ArgumentList "/i","`"$installerPath`"","/quiet","/norestart","TARGETDIR=`"$env:WORK_SPACE/Tailscale`"" -Wait
 Remove-Item $installerPath -Force
-Get-ChildItem -Path "C:\Program Files\Tailscale" -File -Recurse
+Get-ChildItem -Path "$env:WORK_SPACE/Tailscale" -File -Recurse

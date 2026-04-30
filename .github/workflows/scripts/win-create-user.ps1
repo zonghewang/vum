df91a007ddf8c6c933d889e8f1ef67c2e620c31d
zonghewang <diaozhaofeng@126.com>
2026-04-30 19:14:37 +0800

    Update win-create-user.ps1

--- .github/workflows/scripts/win-create-user.ps1
+++ .github/workflows/scripts/win-create-user.ps1
@@ -16,5 +16,6 @@ $securePass = ConvertTo-SecureString $password -AsPlainText -Force
 New-LocalUser -Name "vum" -Password $securePass -AccountNeverExpires
 Add-LocalGroupMember -Group "Administrators" -Member "vum"
 Add-LocalGroupMember -Group "Remote Desktop Users" -Member "vum"
+Add-LocalGroupMember -Group "Users" -Member "vum"
 echo "RDP_CREDS=User: vum | Password: in github.secrets.LOGIN_PASSWORD" >> $env:GITHUB_ENV
 if (-not (Get-LocalUser -Name "vum")) { throw "User creation failed" }

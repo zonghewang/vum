de89306d1794337449d81dae94964ccc055818a3
zonghewang <diaozhaofeng@126.com>
2026-04-30 17:31:15 +0800

    Refactor Tailscale installation method to use Chocolatey
    Updated the script to install Tailscale using Chocolatey instead of the MSI installer. Adjusted environment variable settings for the PATH.

--- .github/workflows/scripts/win-install-tailscale.ps1
+++ .github/workflows/scripts/win-install-tailscale.ps1
@@ -1,29 +1,44 @@
 
-# Install Tailscale MSI
-$tsUrl = "https://pkgs.tailscale.com/stable/tailscale-setup-full-1.92.3.exe"
-$installerPath = "$env:TEMP\tailscale.msi"
-$logPath = "$env:TEMP\tailscale-install.log"
-Invoke-WebRequest -Uri $tsUrl -OutFile $installerPath
-Start-Process msiexec.exe -ArgumentList "/i `"$installerPath`" /quiet /norestart /log `"$logPath`" TARGETDIR=`"$env:WORK_SPACE\Tailscale`"" -Wait -NoNewWindow
+# # Install Tailscale MSI
+# $tsUrl = "https://pkgs.tailscale.com/stable/tailscale-setup-full-1.92.3.exe"
+# $installerPath = "$env:TEMP\tailscale.msi"
+# $logPath = "$env:TEMP\tailscale-install.log"
+# Invoke-WebRequest -Uri $tsUrl -OutFile $installerPath
+# Start-Process msiexec.exe -ArgumentList "/i `"$installerPath`" /quiet /norestart /log `"$logPath`" TARGETDIR=`"$env:WORK_SPACE\Tailscale`"" -Wait -NoNewWindow
 
-# 等待安装完成后检查结果
-$exitCode = $LASTEXITCODE
+# # 等待安装完成后检查结果
+# $exitCode = $LASTEXITCODE
 
-# 根据退出代码判断安装是否成功
-if ($exitCode -eq 0) {
-    Write-Output "安装成功"
-} else {
-    Write-Output "安装失败，退出代码: $exitCode"
-}
+# # 根据退出代码判断安装是否成功
+# if ($exitCode -eq 0) {
+#     Write-Output "安装成功"
+# } else {
+#     Write-Output "安装失败，退出代码: $exitCode"
+# }
 
-# 可选: 检查和显示安装日志的最后几行内容
-if (Test-Path $logPath) {
-    $logContent = Get-Content $logPath -Tail 50
-    Write-Output "日志内容:"
-    Write-Output $logContent
-} else {
-    Write-Output "日志文件不存在"
-}
+# # 可选: 检查和显示安装日志的最后几行内容
+# if (Test-Path $logPath) {
+#     $logContent = Get-Content $logPath -Tail 50
+#     Write-Output "日志内容:"
+#     Write-Output $logContent
+# } else {
+#     Write-Output "日志文件不存在"
+# }
 
-Remove-Item $installerPath -Force
-Get-ChildItem -Path "$env:WORK_SPACE\Tailscale" -File -Recurse
+# Remove-Item $installerPath -Force
+# Get-ChildItem -Path "$env:WORK_SPACE\Tailscale" -File -Recurse
+
+
+choco install -f tailscale
+# [Environment]::SetEnvironmentVariable("Path",$Env:PATH + ";C:\Program Files\Tailscale\",[EnvironmentVariableTarget]::Machine)
+
+$currentPath = [Environment]::GetEnvironmentVariable("PATH", "Machine")
+# Choose one of these next two lines, depending on the WiX priority
+$NewPath = "$currentPath;C:\Program Files\Tailscale\"
+#$NewPath = "$currentPath;$($Env:WIX)\bin"
+
+[Environment]::SetEnvironmentVariable("PATH", $NewPath, "Machine")
+
+refrashenv
+
+$env:PATH -split ';' | ForEach-Object { $_ }

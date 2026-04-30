49421160fbf7d1e01873118f1407b629a5788907
zonghewang <diaozhaofeng@126.com>
2026-04-30 19:29:10 +0800

    Change Tailscale exit node to hostname

--- .github/workflows/scripts/win-establish-ts.ps1
+++ .github/workflows/scripts/win-establish-ts.ps1
@@ -1,6 +1,6 @@
 # Bring up Tailscale and get IP
 
-& tailscale up --authkey=$env:TAILSCALE_AUTH_KEY --hostname=gh-runner-win-vum --exit-node=100.64.113.11
+& tailscale up --authkey=$env:TAILSCALE_AUTH_KEY --hostname=gh-runner-win-vum --exit-node=gh-runner-win-vum
 
 
 $tsIP = $null
